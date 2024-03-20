20a0bb51af85bb6c13ca0423b97ffaa059d82a51
Muhammad Bilal <mohammadbillal268@gmail.com>
2024-03-20 17:17:43 +0500

    output added

---
+++ dart_assignments/assignment5/README.md
@@ -0,0 +1,5 @@
+# Assignment 05
+
+# Output
+
+<img src="assignment 05.jpg">
--- dart_assignments/assignment5/index.dart
+++ dart_assignments/assignment5/index.dart
@@ -2,46 +2,46 @@ void main() {
   // Q.1: Write a Dart program to create a list of your favorite fruits and print them in alphabetical order.
   List<String> arr = ["Banana", "Apple", "Strawberry", "Mango"];
   arr.sort();
-  print('List has soerted in alphabatical order ${arr}');
+  print('Question No 01 : ${arr}');
 
   // Q. 2 Create a list of even numbers from 1 to 20 and print them.
   List<int> eventNum = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-  print('${eventNum}');
+  print('Question No 02 : ${eventNum}');
 
   // Q.3: Write a Dart program to remove all duplicate elements from a given list of numbers.
   List<int> evenNumDuplicate = [2, 4, 6, 8, 4, 8];
   List<int> uniqueArr = evenNumDuplicate.toSet().toList();
-  print(uniqueArr);
+  print("Question No 03 :  ${uniqueArr}");
 
   // Q.4: Write a Dart program to check if a given list is empty or not.
   List arr1 = [2];
   int arr1Length = arr1.length;
   print('${arr1Length}');
   if (arr1Length > 0) {
-    print('array is not empty');
+    print('Question No 04 : array is not empty');
   } else {
-    print('array is empty');
+    print('Question No 04 : array is empty');
   }
-  bool arrayEmpty = arr1.isEmpty;
-  if (arrayEmpty) {
-    print('array is empty');
-  } else {
-    print('array is not empty');
-  }
+  // bool arrayEmpty = arr1.isEmpty;
+  // if (arrayEmpty) {
+  //   print('array is empty');
+  // } else {
+  //   print('array is not empty');
+  // }
 
   // Q.5: Create two lists of numbers and merge them into a single list. Print the merged list.
 
   List<int> num1 = [3, 5, 7];
   List<int> num2 = [2, 4, 6];
   List mergeList = num1 + num2;
-  print("${mergeList}");
+  print("Question No 05 : ${mergeList}");
 
   // Q.6: Write a Dart program to create a map representing a student's information including name, age, and grade. Print out all the information stored in the map.
 
   Map student = {"name": "Bilal", "age": 28, "grade": "A"};
-  print("${student['name']}");
-  print("${student['age']}");
-  print("${student['grade']}");
+  print("Question No 06 : ${student['name']}");
+  print("Question No 06 : ${student['age']}");
+  print("Question No 06 : ${student['grade']}");
 
   // Q.8: Create a map representing the population of different cities. Write a Dart program to find and print the city with the highest population.
 
@@ -54,8 +54,8 @@ void main() {
   };
 
   List sortedList = citiesPopulation.values.toList();
-  print(sortedList);
+  // print(sortedList);
   sortedList.sort();
-  print(sortedList);
-  print(sortedList.last);
+  // print(sortedList);
+  print("Question No 08 : ${sortedList.last}");
 }
